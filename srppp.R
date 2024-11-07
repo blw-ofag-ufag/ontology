10b2111aa993068e99fdef7d2a28f2746c2d0f56
Damian-Oswald <damian.oswald@protonmail.com>
2024-11-07 15:18:07 +0100

    update ingredient taxonomy

--- srppp.R
+++ srppp.R
@@ -10,3 +10,12 @@ current_register$CodeS
 current_register$CodeS$CodeS_de |> unique()
 current_register$CodeR$CodeR_de |> unique()
 current_register$application_comments$application_comment_de |> unique()
+current_register$obligations$code |> unique()
+current_register$obligations$obligation_de |> unique()
+
+
+
+current_register$products
+current_register$ingredients
+current_register$formulation_codes
+current_register$substances
