8d676a8bfa198f62aae24ff47360d10bfce8b01d
Damian-Oswald <damian.oswald@protonmail.com>
2024-11-08 11:08:07 +0100

    Change :Pest labels
    :Pest class labels now match the Swiss registry for plant protection.

--- srppp.R
+++ srppp.R
@@ -11,11 +11,18 @@ current_register$CodeS$CodeS_de |> unique()
 current_register$CodeR$CodeR_de |> unique()
 current_register$application_comments$application_comment_de |> unique()
 current_register$obligations$code |> unique()
-current_register$obligations$obligation_de |> unique()
 
+sink("obligations.txt")
+current_register$obligations$obligation_de |> unique() |> print()
+sink()
 
 
 current_register$products
 current_register$ingredients
 current_register$formulation_codes
 current_register$substances
+
+current_register$uses |> View()
+current_register$uses$units_de |> unique()
+current_register$cultures$culture_de |> unique()
+current_register$culture_forms
