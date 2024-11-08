177debc6b2f42c3a2460ede692bb39fbe21a2862
Damian-Oswald <damian.oswald@protonmail.com>
2024-11-08 17:10:41 +0100

    Update ontology

--- srppp.R
+++ srppp.R
@@ -13,7 +13,12 @@ current_register$application_comments$application_comment_de |> unique()
 current_register$obligations$code |> unique()
 
 sink("obligations.txt")
-current_register$obligations$obligation_de |> unique() |> print()
+x = current_register$obligations$obligation_de |>
+  table() |>
+  sort(decreasing = T)
+y = names(x)
+cat("AUFLAGEN PSMV\n\n")
+for (i in 1:length(x)) cat(sprintf("%s: %s (%s)\n", i, y[i], x[i]))
 sink()
 
 
@@ -26,3 +31,13 @@ current_register$uses |> View()
 current_register$uses$units_de |> unique()
 current_register$cultures$culture_de |> unique()
 current_register$culture_forms
+
+
+
+# Code S to RDF
+X = as.data.frame(unique(current_register$CodeS[,4:6]))
+for (i in 1:nrow(X)) {
+  cat(sprintf("\n:codeS%s a :CodeS ; \n\trdfs:label \"%s\"@de , \n\t\t\"%s\"@fr , \n\t\t\"%s\"@it .", i, X[i,1], X[i,2], X[i,3]))
+}
+
+
