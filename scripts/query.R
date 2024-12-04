70d6e5ca9e5119bc992eb6ca203770625b1c262e
Damian-Oswald <damian.oswald@protonmail.com>
2024-12-04 18:17:09 +0100

    add data

---
+++ scripts/query.R
@@ -0,0 +1,54 @@
+# add libraries to search path
+library(rdflib)
+library(jsonld)
+
+# EXAMPLE 1 : READ THE ONTOLOGY FILE DIRECTLY ----------------------------------------------------------
+
+# set path to RDF file
+RDF = rdf_parse("/home/damian.oswald@blw.admin.ch/ontology/plant-protection.ttl")
+
+# define SPARQL query
+SPARQL = '
+PREFIX : <https://raw.githubusercontent.com/blw-ofag-ufag/ontology/refs/heads/main/plant-protection.ttl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT ?productName ?code ?companyName ?substanceName ?parallel ?nonProfessional
+WHERE {
+  ?product a :Product .
+  ?product rdfs:label ?productName .
+  ?product :hasFederalRegistrationCode ?code .
+  OPTIONAL {
+    ?company :holdsPermissionToSell ?product .
+    ?company rdfs:label ?companyName .
+  }
+  OPTIONAL { 
+    ?product :contains ?substance .
+    ?substance rdfs:label ?substanceName .
+  }
+  OPTIONAL { ?product :isParallelImport ?parallel . }
+  OPTIONAL { ?product :isNonprofessionallyAllowed ?nonProfessional . }
+}
+'
+
+# run SPARQL query
+rdf_query(RDF, SPARQL)
+
+# EXAMPLE 2 : READ THE DATA FILE -------------------------------------------------------------------------
+
+# set path to RDF file
+RDF = rdf_parse("ontology/data.ttl")
+
+# define SPARQL query
+SPARQL = '
+PREFIX : <https://raw.githubusercontent.com/blw-ofag-ufag/ontology/refs/heads/main/plant-protection.ttl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT ?company (COUNT(DISTINCT ?product) AS ?productCount)
+WHERE {
+  ?product a :Product .
+  ?product :hasPermissionHolder ?company .
+}
+GROUP BY ?company
+ORDER BY DESC(?productCount)  # Optional, orders by the number of products in descending order
+'
+
+# run SPARQL query
+rdf_query(RDF, SPARQL)
